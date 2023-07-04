3b1e2ccf2c6e59c52fbdf61a22df8ddb89d01baf
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-07-04 11:00:33 +0200

    add cdn url for assets

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.js', 'api.ts'],
   output: 'standalone',
   basePath: '/syk/mer-oppfolging-wip',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig
