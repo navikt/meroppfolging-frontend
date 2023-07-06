a111fa47e8cf8436267044cd24b4acfd32e9cdf0
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-07-06 15:01:06 +0200

    add trpc and dummy backend router

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from 'react'
 
 import { withAuthenticatedPage } from '@/auth/withAuthentication'
+import { trpc } from '@/utils/trpc'
 
 function Home(): ReactElement {
-  return <main>Buuu</main>
+  const hello = trpc.hello.useQuery({ text: 'client' })
+  if (!hello.data) {
+    return <div>Loading...</div>
+  }
+  return (
+    <div>
+      <p>{hello.data.greeting}</p>
+    </div>
+  )
 }
 
 export const getServerSideProps = withAuthenticatedPage()
