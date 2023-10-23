272e4d554741600fa3bdba6a9d5e6e6ea932a334
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-10-23 22:43:21 +0200

    fix registration payload mapper

--- src/server/services/schemas/registreringSchema.ts
+++ src/server/services/schemas/registreringSchema.ts
@@ -35,52 +35,87 @@ export const IKKE_BESVART = 'Ikke besvart'
 const ingenSvarLiteral = z.literal(INGEN_SVAR)
 const ikkeBesvartLiteral = z.literal(IKKE_BESVART)
 
+const teksterForBesvarelseSchema = z.tuple([
+  z.object({
+    sporsmalId: z.literal(QuestionId.utdanning),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.utdanning]),
+    svar: z.nativeEnum(utdanningAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.utdanningGodkjent),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.utdanningGodkjent]),
+    svar: z.nativeEnum(utdanningGodkjentAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.utdanningBestatt),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.utdanningBestatt]),
+    svar: z.nativeEnum(utdanningBestattAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.andreForhold),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.andreForhold]),
+    svar: z.nativeEnum(andreForholdAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.fremtidigSituasjon),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.fremtidigSituasjon]),
+    svar: z.nativeEnum(fremtidigSituasjonAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal('sisteStilling'),
+    sporsmal: z.literal('Hva er din siste jobb?'),
+    svar: z.literal('Ikke oppgitt'),
+  }),
+])
+
+const teksterForBesvarelseWithTilbakeIArbeidSchema = z.tuple([
+  z.object({
+    sporsmalId: z.literal(QuestionId.utdanning),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.utdanning]),
+    svar: z.nativeEnum(utdanningAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.utdanningGodkjent),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.utdanningGodkjent]),
+    svar: z.nativeEnum(utdanningGodkjentAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.utdanningBestatt),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.utdanningBestatt]),
+    svar: z.nativeEnum(utdanningBestattAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.andreForhold),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.andreForhold]),
+    svar: z.nativeEnum(andreForholdAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.fremtidigSituasjon),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.fremtidigSituasjon]),
+    svar: z.nativeEnum(fremtidigSituasjonAlt).or(ikkeBesvartLiteral),
+  }),
+  z.object({
+    sporsmalId: z.literal('sisteStilling'),
+    sporsmal: z.literal('Hva er din siste jobb?'),
+    svar: z.literal('Ikke oppgitt'),
+  }),
+  z.object({
+    sporsmalId: z.literal(QuestionId.tilbakeIArbeid),
+    sporsmal: z.literal(formQuestionTexts[QuestionId.tilbakeIArbeid]),
+    svar: z.nativeEnum(tilbakeIArbeidAlt).or(ikkeBesvartLiteral),
+  }),
+])
+
 export const completeRegistrationSchema = z.object({
   besvarelse: z.object({
     [QuestionId.utdanning]: z.nativeEnum(UtdanningValues).or(ingenSvarLiteral),
     [QuestionId.utdanningGodkjent]: z.nativeEnum(UtdanningGodkjentValues).or(ingenSvarLiteral),
     [QuestionId.utdanningBestatt]: z.nativeEnum(UtdanningBestattValues).or(ingenSvarLiteral),
     [QuestionId.andreForhold]: z.nativeEnum(AndreForholdValues).or(ingenSvarLiteral),
-    [QuestionId.fremtidigSituasjon]: z.nativeEnum(FremtidigSituasjonValues).or(ingenSvarLiteral),
-    [QuestionId.tilbakeIArbeid]: z.nativeEnum(TilbakeIArbeidValues).or(ingenSvarLiteral),
+    [QuestionId.fremtidigSituasjon]: z.optional(z.nativeEnum(FremtidigSituasjonValues)),
+    [QuestionId.tilbakeIArbeid]: z.optional(z.nativeEnum(TilbakeIArbeidValues)),
     sisteStilling: ingenSvarLiteral,
   }),
-  teksterForBesvarelse: z.tuple([
-    z.object({
-      sporsmalId: z.literal(QuestionId.utdanning),
-      sporsmal: z.literal(formQuestionTexts[QuestionId.utdanning]),
-      svar: z.nativeEnum(utdanningAlt).or(ikkeBesvartLiteral),
-    }),
-    z.object({
-      sporsmalId: z.literal(QuestionId.utdanningGodkjent),
-      sporsmal: z.literal(formQuestionTexts[QuestionId.utdanningGodkjent]),
-      svar: z.nativeEnum(utdanningGodkjentAlt).or(ikkeBesvartLiteral),
-    }),
-    z.object({
-      sporsmalId: z.literal(QuestionId.utdanningBestatt),
-      sporsmal: z.literal(formQuestionTexts[QuestionId.utdanningBestatt]),
-      svar: z.nativeEnum(utdanningBestattAlt).or(ikkeBesvartLiteral),
-    }),
-    z.object({
-      sporsmalId: z.literal(QuestionId.andreForhold),
-      sporsmal: z.literal(formQuestionTexts[QuestionId.andreForhold]),
-      svar: z.nativeEnum(andreForholdAlt).or(ikkeBesvartLiteral),
-    }),
-    z.object({
-      sporsmalId: z.literal(QuestionId.fremtidigSituasjon),
-      sporsmal: z.literal(formQuestionTexts[QuestionId.fremtidigSituasjon]),
-      svar: z.nativeEnum(fremtidigSituasjonAlt).or(ikkeBesvartLiteral),
-    }),
-    z.object({
-      sporsmalId: z.literal(QuestionId.tilbakeIArbeid),
-      sporsmal: z.literal(formQuestionTexts[QuestionId.tilbakeIArbeid]),
-      svar: z.nativeEnum(tilbakeIArbeidAlt).or(ikkeBesvartLiteral),
-    }),
-    z.object({
-      sporsmalId: z.literal('sisteStilling'),
-      sporsmal: z.literal('Hva er din siste jobb?'),
-      svar: z.literal('Ikke oppgitt'),
-    }),
-  ]),
+  teksterForBesvarelse: z.union([teksterForBesvarelseSchema, teksterForBesvarelseWithTilbakeIArbeidSchema]),
 })
 export type CompleteRegistrationRequest = z.infer<typeof completeRegistrationSchema>
