a5956bf9e6ccf0299d0b1bc0c9e855f2fb7e1649
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-11-06 09:46:06 +0100

    add submit form integration tests

--- src/pages/reg/form.test.tsx
+++ src/pages/reg/form.test.tsx
@@ -1,6 +1,5 @@
 import mockRouter from 'next-router-mock'
 import { beforeEach, describe, expect, it } from 'vitest'
-import { within } from '@testing-library/react'
 
 import FormPage from '@/pages/reg/[form].page'
 import { render, screen } from '@/test/testUtils'
@@ -22,7 +21,7 @@ describe('FormPage', () => {
 
     it('should render maintenance info', async () => {
       testServer.use(
-        trpcMsw.featureToggles.query(async (req, res, ctx) => {
+        trpcMsw.featureToggles.query(async (_req, res, ctx) => {
           return res(ctx.status(200), ctx.data(enabledFeatureToggles))
         }),
       )
@@ -30,34 +29,6 @@ describe('FormPage', () => {
       render(<FormPage />)
 
       expect(await screen.findByText('Vedlikehold pågår')).toBeInTheDocument()
-    })
-  })
-
-  describe('filling out form', () => {
-    it('should submit form', async () => {
-      const { user } = render(<FormPage />)
-
-      const radioGroup1 = within(
-        await screen.findByRole('group', {
-          name: 'Hva tenker du om din fremtidige situasjon?',
-        }),
-      )
-      await user.click(radioGroup1.getByLabelText('Jeg skal tilbake til jobben jeg har'))
-      await user.click(screen.getByRole('button', { name: 'Neste' }))
-
-      const radioGroup2 = within(
-        await screen.findByRole('group', {
-          name: 'Tror du at du kommer tilbake i jobb før du har vært sykmeldt i 52 uker?',
-        }),
-      )
-      await user.click(radioGroup2.getByLabelText('Ja, i full stilling'))
-      await user.click(screen.getByRole('button', { name: 'Neste' }))
-
-      await user.click(screen.getByRole('button', { name: 'Uenig, jeg trenger mer veiledning' }))
-
-      const summaryTable = within(await screen.findByRole('table', { name: 'Sammendrag av svar' }))
-      summaryTable.getByRole('link', { name: 'Endre svaret på Fremtidig situasjon' })
-      summaryTable.getByRole('link', { name: 'Endre svaret på Tilbake i jobb før sykmeldt i 52 uker' })
     })
   })
 })
