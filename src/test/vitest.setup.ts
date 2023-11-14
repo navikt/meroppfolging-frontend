bcead57853ea33262d70ec04708d06ef96c73de3
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-11-14 13:44:32 +0100

    add axe testing

--- src/test/vitest.setup.ts
+++ src/test/vitest.setup.ts
@@ -3,6 +3,7 @@ import { vi, beforeAll, afterEach, afterAll } from 'vitest'
 import dotenv from 'dotenv'
 import { expect } from 'vitest'
 import * as matchers from 'vitest-dom/matchers'
+import * as vitestAxeMatchers from 'vitest-axe/matchers'
 import { cleanup } from '@testing-library/react'
 
 import { testServer } from '../mocks/testServer'
@@ -10,6 +11,7 @@ import { testServer } from '../mocks/testServer'
 import 'vitest-dom/extend-expect'
 
 expect.extend(matchers)
+expect.extend(vitestAxeMatchers)
 
 dotenv.config({
   path: '.env.test',
