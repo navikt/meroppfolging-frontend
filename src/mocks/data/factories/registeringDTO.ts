e1e3feef75fa0145705857a072e3d4720a5f881c
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-03-04 08:23:01 +0100

    Test logging of response from veilarbregistrering

--- src/mocks/data/factories/registeringDTO.ts
+++ src/mocks/data/factories/registeringDTO.ts
@@ -3,6 +3,9 @@ import { StartRegistrationDTO } from '@/server/services/schemas/registreringSche
 export function createStartRegisterinDTO(overrides?: Partial<StartRegistrationDTO>): StartRegistrationDTO {
   return {
     registreringType: 'SYKMELDT_REGISTRERING',
+    formidlingsgruppe: 'IARBS',
+    rettighetsgruppe: 'IYT',
+    servicegruppe: 'VURDI',
     ...overrides,
   }
 }
