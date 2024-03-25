9115dbf115abab76d739f123c271c0fa9e0d92a5
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-03-25 23:34:26 +0100

    migrate auth to oasis

--- src/auth/tokenUtils.ts
+++ src/auth/tokenUtils.ts
@@ -1,39 +1,39 @@
-import { grantTokenXOboToken, isInvalidTokenSet } from '@navikt/next-auth-wonderwall'
+import { requestOboToken } from '@navikt/oasis'
 
 import { getServerEnv } from '@/constants/envs'
 
 export async function exchangeIdportenTokenForVeilarbregisteringTokenx(auth: string): Promise<string> {
   const VEILARBREGISTRERING_CLIENT_ID = `${getServerEnv().NAIS_CLUSTER_NAME}:paw:veilarbregistrering`
 
-  const tokenxGrant = await grantTokenXOboToken(auth, VEILARBREGISTRERING_CLIENT_ID)
+  const tokenxGrant = await requestOboToken(auth, VEILARBREGISTRERING_CLIENT_ID)
 
-  if (isInvalidTokenSet(tokenxGrant)) {
-    throw new Error(`Failed to exchange idporten token for veilarbtokenx: ${tokenxGrant.message}`)
+  if (!tokenxGrant.ok) {
+    throw new Error(`Failed to exchange idporten token for veilarbtokenx: ${tokenxGrant.error}`)
   }
 
-  return tokenxGrant
+  return tokenxGrant.token
 }
 
 export async function exchangeIdportenTokenForEsyfoVarselTokenx(auth: string): Promise<string> {
   const ESYFO_VARSEL_CLIENT_ID = `${getServerEnv().NAIS_CLUSTER_NAME}:team-esyfo:esyfovarsel`
 
-  const tokenxGrant = await grantTokenXOboToken(auth, ESYFO_VARSEL_CLIENT_ID)
+  const tokenxGrant = await requestOboToken(auth, ESYFO_VARSEL_CLIENT_ID)
 
-  if (isInvalidTokenSet(tokenxGrant)) {
-    throw new Error(`Failed to exchange idporten token for esyfovarseltokenx: ${tokenxGrant.message}`)
+  if (!tokenxGrant.ok) {
+    throw new Error(`Failed to exchange idporten token for esyfovarseltokenx: ${tokenxGrant.error}`)
   }
 
-  return tokenxGrant
+  return tokenxGrant.token
 }
 
 export async function exchangeIdportenTokenForMeroppfolgingBackendTokenx(auth: string): Promise<string> {
   const MEROPPFOLGING_BACKEND_CLIENT_ID = `${getServerEnv().NAIS_CLUSTER_NAME}:team-esyfo:meroppfolging-backend`
 
-  const tokenxGrant = await grantTokenXOboToken(auth, MEROPPFOLGING_BACKEND_CLIENT_ID)
+  const tokenxGrant = await requestOboToken(auth, MEROPPFOLGING_BACKEND_CLIENT_ID)
 
-  if (isInvalidTokenSet(tokenxGrant)) {
-    throw new Error(`Failed to exchange idporten token for meroppfolging-backend tokenx: ${tokenxGrant.message}`)
+  if (!tokenxGrant.ok) {
+    throw new Error(`Failed to exchange idporten token for meroppfolging-backend tokenx: ${tokenxGrant.error}`)
   }
 
-  return tokenxGrant
+  return tokenxGrant.token
 }
