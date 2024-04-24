b6029a9c8d56c0810fbba019fa9573d3acea553a
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-04-24 13:44:43 +0200

    refactor from url object to string

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,7 @@
-import { UrlObject } from 'url'
-
 import { formPageInverted } from '@/domain/formPages'
 import { FormPage } from '@/types/merOppfolgingForm'
 import { FORM_PATH } from '@/constants/paths'
 
-export function getFormUrlObject(form: FormPage): UrlObject {
-  return {
-    pathname: FORM_PATH,
-    query: { form: formPageInverted[form] },
-  }
+export function getFormUrl(form: FormPage): string {
+  return `${FORM_PATH}/${formPageInverted[form]}`
 }
