34e82278ca1d33f438d6b12ffa87250791367ba3
AudunSorheim <audun.sorheim@nav.no>
2024-05-30 10:04:39 +0200

    wip

--- src/pages/api/senoppfolging/status.api.ts
+++ src/pages/api/senoppfolging/status.api.ts
@@ -2,16 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { isLocalOrDemo } from '@/constants/envs'
 import { beskyttetApi } from '@/auth/beskyttetApi'
-import { SenOppfolgingStatusDTOV2 } from '@/server/services/schemas/statusSchemaV2'
 import { getStatusV2 } from '@/server/services/meroppfolgingService'
+import { senOppfolgingStatusDTOV2Fixture } from '@/mocks/data/fixtures/senOppfolgingStatusDTOV2Fixture'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   if (isLocalOrDemo) {
-    const mockStatus: SenOppfolgingStatusDTOV2 = {
-      isPilot: true,
-      responseStatus: 'TRENGER_OPPFOLGING',
-    }
-    res.status(200).json(mockStatus)
+    res.status(200).json(senOppfolgingStatusDTOV2Fixture)
   } else {
     const status = await getStatusV2(req)
     res.status(200).json(status)
