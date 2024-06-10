f1020aa69c63191f5079f7cef8217443341aba74
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-06-10 15:16:07 +0200

    Add "contact us"-link

--- src/pilot/components/Receipt/ContactUs.tsx
+++ src/pilot/components/Receipt/ContactUs.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from 'react'
 import { BodyShort, Heading } from '@navikt/ds-react'
+import Link from 'next/link'
+
+import { CONTACT_NAV_URL } from '@/constants/paths'
 
 function ContactUs(): ReactElement {
   return (
     <>
       <Heading size="medium">Kontakt oss</Heading>
-      <BodyShort>Ta gjerne kontakt med oss hvis du trenger å snakke med en veileder i NAV</BodyShort>
+      <BodyShort>
+        Ta gjerne <Link href={CONTACT_NAV_URL}>kontakt med oss</Link> hvis du trenger å snakke med en veileder i NAV
+      </BodyShort>
     </>
   )
 }
