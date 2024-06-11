b24eebb20bb8c4c363d0dc05710876f3aa6ecb09
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-06-11 12:59:19 +0200

    Some minor styling

--- src/pilot/components/Receipt/UsefulLinks.tsx
+++ src/pilot/components/Receipt/UsefulLinks.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import { BodyLong, Box, Heading, Link } from '@navikt/ds-react'
+import { Box, Heading, Link } from '@navikt/ds-react'
 import { ExternalLinkIcon } from '@navikt/aksel-icons'
 
 function UsefulLinks(): ReactElement {
@@ -8,26 +8,28 @@ function UsefulLinks(): ReactElement {
       <Heading size="medium" level="2" spacing>
         Nyttige lenker
       </Heading>
-      <BodyLong>
-        <Link href="https://www.nav.no/syk-lenge">
-          Har vært syk eller skadet lenge <ExternalLinkIcon title="external link" />
-        </Link>
-      </BodyLong>
-      <BodyLong>
-        <Link href="https://www.nav.no/aap">
-          Arbeidsavklaringspenger (AAP) <ExternalLinkIcon title="external link" />
-        </Link>
-      </BodyLong>
-      <BodyLong>
-        <Link href="https://www.nav.no/arbeidsgiver/kompetansetiltak-sykmeldte">
-          Kompetansetiltak for sykmeldte <ExternalLinkIcon title="external link" />
-        </Link>
-      </BodyLong>
-      <BodyLong>
-        <Link href="https://www.nav.no/friskmelding-arbeidsformidling">
-          Friskmelding til arbeidsformidling <ExternalLinkIcon title="external link" />
-        </Link>
-      </BodyLong>
+      <ul>
+        <li>
+          <Link href="https://www.nav.no/syk-lenge">
+            Har vært syk eller skadet lenge <ExternalLinkIcon title="external link" />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.nav.no/aap">
+            Arbeidsavklaringspenger (AAP) <ExternalLinkIcon title="external link" />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.nav.no/arbeidsgiver/kompetansetiltak-sykmeldte">
+            Kompetansetiltak for sykmeldte <ExternalLinkIcon title="external link" />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.nav.no/friskmelding-arbeidsformidling">
+            Friskmelding til arbeidsformidling <ExternalLinkIcon title="external link" />
+          </Link>
+        </li>
+      </ul>
     </Box>
   )
 }
