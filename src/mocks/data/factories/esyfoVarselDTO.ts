9ce4bc20ae3b17fc42b2579b818a0d6584973f68
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-08-20 14:43:38 +0200

    use gjennstaaende dager (#550)

--- src/mocks/data/factories/esyfoVarselDTO.ts
+++ src/mocks/data/factories/esyfoVarselDTO.ts
@@ -4,6 +4,7 @@ export function createMaxDateDTO(overrides?: Partial<MaxDateDTO>): MaxDateDTO {
   return {
     maxDate: '1. november 2023',
     utbetaltTom: '4. april 2023',
+    gjenstaendeSykedager: '70',
     ...overrides,
   }
 }
