b4381f6c252056b3c66768ef97f6a4a0879e5020
Håkon Møller <hakon.salomonsen.moller@nav.no>
2024-11-01 12:14:01 +0100

    Show no access information for certain users (#700)
    Show no access information for users that should not have access. The concern now is users that have not received a notification but still visits the URL.
    
    Also add a 404 page for users that visits a random URL.
    
    https://trello.com/c/SdkQDFwD/1322-meroppfolging-frontend-vis-404-side-for-sykmeldte-som-bes%C3%B8ker-skjema-landingssiden-og-ikke-har-f%C3%A5tt-varsel
    
    Co-authored-by: Luan Tran <luan.tran@nav.no>

--- src/pilot/components/LandingPage/LandingPilot.tsx
+++ src/pilot/components/LandingPage/LandingPilot.tsx
@@ -4,21 +4,26 @@ import { VStack } from '@navikt/ds-react'
 import PageContainer from '@/pilot/components/containers/PageContainer'
 import SenOppfolging from '@/pilot/components/SenOppfolging/SenOppfolging'
 import Receipt from '@/pilot/components/Receipt/Receipt'
-import { PilotStatus } from '@/server/services/schemas/statusSchema'
+import { StatusPilotDTO } from '@/server/services/schemas/statusSchema'
+import NoAccessInformation from '@/pilot/components/LandingPage/NoAccessInformation'
 
-function LandingContent({ response }: { response: PilotStatus['response'] }): ReactElement {
-  if (response === null) {
+function LandingContent({ status }: { status: StatusPilotDTO }): ReactElement {
+  if (!status.hasAccessToSenOppfolging) {
+    return <NoAccessInformation />
+  }
+
+  if (status.response === null) {
     return <SenOppfolging />
   } else {
-    return <Receipt response={response} />
+    return <Receipt response={status.response} />
   }
 }
 
-function LandingPilot({ status }: { status: PilotStatus }): ReactElement {
+function LandingPilot({ status }: { status: StatusPilotDTO }): ReactElement {
   return (
     <PageContainer className="bg-bg-subtle">
       <VStack gap="6" className="max-w-4xl bg-bg-default px-4 py-8 md:p-12">
-        <LandingContent response={status.response} />
+        <LandingContent status={status} />
       </VStack>
     </PageContainer>
   )
