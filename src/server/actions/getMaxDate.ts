42206d5504b685df9fa328bcb6ed90fefaeba2a7
Audun Sørheim <audun.sorheim@nav.no>
2025-02-21 14:39:31 +0100

    Add error handling

--- src/server/actions/getMaxDate.ts
+++ src/server/actions/getMaxDate.ts
@@ -1,7 +1,12 @@
 import { MaxDateDTO, maxDateSchema } from '@/server/schemas/sykepengedagerInformasjonSchema'
 import { getServerEnv, isLocalOrDemo } from '@/constants/envs'
+import { exchangeIdportenTokenForSykepengedagerInformasjonTokenx } from '@/auth/tokenUtils'
+import { headers } from 'next/headers'
+import { getToken } from '@navikt/oasis'
 import { maxDateDTO } from '@/mocks/data/fixtures/sykepengedagerInformasjonDTO'
-import { fetchFromMobe } from '@/server/actions/fetch'
+import { nanoid } from 'nanoid'
+import { validateIdPortenToken } from '@/auth/getIdPortenToken'
+import { navigateToLogin } from '@/auth/navigateToLogin'
 import { logger } from '@navikt/next-logger'
 
 export async function getMaxDate(): Promise<MaxDateDTO> {
@@ -10,7 +15,22 @@ export async function getMaxDate(): Promise<MaxDateDTO> {
   }
 
   const endpoint = `${getServerEnv().SYKEPENGEDAGER_INFORMASJON_MAX_DATE_API_URL}?isoformat=true`
-  const response = await fetchFromMobe(endpoint)
+  const headersList = await headers()
+  const isAuthenticated = await validateIdPortenToken()
+  if (!isAuthenticated) {
+    navigateToLogin()
+  }
+  const idportenToken = getToken(headersList)
+  const exchangedToken = await exchangeIdportenTokenForSykepengedagerInformasjonTokenx(idportenToken)
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${exchangedToken}`,
+      'Nav-Consumer-Id': 'meroppfolging-frontend',
+      'Nav-Call-Id': nanoid(),
+      'Content-Type': 'application/json',
+    },
+  })
 
   if (!response.ok) {
     logger.error(`Failed to fetch data from ${endpoint}: ${response.statusText}`)
@@ -21,7 +41,7 @@ export async function getMaxDate(): Promise<MaxDateDTO> {
   const parsed = maxDateSchema.safeParse(data)
 
   if (!parsed.success) {
-    logger.error('Failed to parse reponse ' + parsed.error)
+    logger.error('Failed to parse result! ' + parsed.error)
     throw parsed.error
   }
 
--- src/server/actions/senOppfolgingStatus.ts
+++ src/server/actions/senOppfolgingStatus.ts
@@ -2,9 +2,14 @@
 
 import { SenOppfolgingStatusDTO, SenOppfolgingStatusSchema } from '@/server/schemas/statusSchema'
 import { getServerEnv, isLocalOrDemo } from '@/constants/envs'
+import { headers } from 'next/headers'
+import { getToken } from '@navikt/oasis'
+import { exchangeIdportenTokenForMeroppfolgingBackendTokenx } from '@/auth/tokenUtils'
 import * as statusDtoFixtures from '@/mocks/data/fixtures/statusDtoFixtures'
+import { nanoid } from 'nanoid'
+import { validateIdPortenToken } from '@/auth/getIdPortenToken'
+import { navigateToLogin } from '@/auth/navigateToLogin'
 import { logger } from '@navikt/next-logger'
-import { fetchFromMobe } from '@/server/actions/fetch'
 
 export async function senOppfolgingStatus(): Promise<SenOppfolgingStatusDTO> {
   if (isLocalOrDemo) {
@@ -12,7 +17,22 @@ export async function senOppfolgingStatus(): Promise<SenOppfolgingStatusDTO> {
   }
 
   const endpoint = `${getServerEnv().MEROPPFOLGING_BACKEND_URL}/api/v2/senoppfolging/status`
-  const response = await fetchFromMobe(endpoint)
+  const headersList = await headers()
+  const isAuthenticated = await validateIdPortenToken()
+  if (!isAuthenticated) {
+    navigateToLogin()
+  }
+  const idportenToken = getToken(headersList)
+  const exchangedToken = await exchangeIdportenTokenForMeroppfolgingBackendTokenx(idportenToken)
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${exchangedToken}`,
+      'Nav-Consumer-Id': 'meroppfolging-frontend',
+      'Nav-Call-Id': nanoid(),
+      'Content-Type': 'application/json',
+    },
+  })
 
   if (!response.ok) {
     logger.error(`Failed to fetch data from ${endpoint}: ${response.statusText}`)
@@ -23,7 +43,7 @@ export async function senOppfolgingStatus(): Promise<SenOppfolgingStatusDTO> {
   const parsed = SenOppfolgingStatusSchema.safeParse(data)
 
   if (!parsed.success) {
-    logger.error('Failed to parse reponse ' + parsed.error)
+    logger.error('Failed to parse result! ' + parsed.error)
     throw parsed.error
   }
 
