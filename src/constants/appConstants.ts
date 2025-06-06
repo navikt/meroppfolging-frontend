926ece4ab8f9a4bb4a57ee7595268f0e2e7940d5
Bjørn Faaberg <bjof@computas.com>
2025-06-06 08:30:10 +0200

    Change url used to contact Nav
    Change from skriv-til-oss to still-sporsmal

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,4 +1,4 @@
 import { basePath } from '../../next.config'
 
 export const BASE_PATH = basePath || ''
-export const CONTACT_NAV_URL = 'https://www.nav.no/kontaktoss#skriv-til-oss'
+export const CONTACT_NAV_URL = 'https://www.nav.no/still-sporsmal'
